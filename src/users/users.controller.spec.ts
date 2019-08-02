227998b6b92edc04a9dbf5ea5519b185879a3c21
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2019-08-02 17:29:55 +0200

    Unit tests for user module

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,24 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { ConfigService } from '../config/config.service';
 import { FetcherService } from '../fetcher/fetcher.service';
+import { Request } from 'express';
+import { NotFoundException } from '@nestjs/common';
+import { UserProfile } from 'github-api-fetcher';
 
-describe('User Controller', (): void => {
+describe('Users Controller', (): void => {
     let controller: UsersController;
+    let userService: UsersService;
 
     beforeEach(
         async (): Promise<void> => {
             const module: TestingModule = await Test.createTestingModule({
                 controllers: [UsersController],
-                providers: [UsersService, FetcherService, ConfigService]
+                providers: [
+                    UsersService,
+                    {
+                        provide: FetcherService,
+                        useValue: { fetch: jest.fn() }
+                    }
+                ]
             }).compile();
 
             controller = module.get<UsersController>(UsersController);
+            userService = module.get<UsersService>(UsersService);
         }
     );
 
     it('should be defined', (): void => {
         expect(controller).toBeDefined();
+    });
+
+    describe('getUser', (): void => {
+        it('should throw NotFoundException when UsersService returns null', async (): Promise<void> => {
+            jest.spyOn(userService, 'getUser').mockReturnValue(new Promise((resolve): void => resolve(null)));
+
+            await expect(controller.getUser(({} as unknown) as Request, 'dummy-username')).rejects.toThrowError(
+                NotFoundException
+            );
+        });
+
+        it('should return result from UsersService', async (): Promise<void> => {
+            const username = 'test-user';
+            const returnValue = ({ username } as unknown) as UserProfile;
+
+            jest.spyOn(userService, 'getUser').mockReturnValue(
+                new Promise(
+                    (resolve): void => {
+                        resolve(returnValue);
+                    }
+                )
+            );
+
+            expect(await controller.getUser(({} as unknown) as Request, username)).toBe(returnValue);
+        });
     });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { FetcherService } from '../fetcher/fetcher.service';
+import { Request } from 'express';
 
 describe('UsersService', (): void => {
     let service: UsersService;
+    let fetchMock: jest.Mock = jest.fn();
 
     beforeEach(
         async (): Promise<void> => {
             const module: TestingModule = await Test.createTestingModule({
-                controllers: [UsersController],
-                providers: [UsersService]
+                providers: [
+                    UsersService,
+                    {
+                        provide: FetcherService,
+                        useValue: {
+                            fetch: fetchMock,
+                            fetcher: { user: { getProfile: jest.fn() } }
+                        }
+                    }
+                ]
             }).compile();
 
             service = module.get<UsersService>(UsersService);
@@ -18,5 +28,13 @@ describe('UsersService', (): void => {
 
     it('should be defined', (): void => {
         expect(service).toBeDefined();
+    });
+
+    describe('getUser', (): void => {
+        it('should call fetch method on FetcherService', async (): Promise<void> => {
+            await service.getUser(({} as unknown) as Request, 'dummy-username');
+
+            expect(fetchMock).toHaveBeenCalled();
+        });
     });
 });
