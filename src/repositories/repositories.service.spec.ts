c29d26b318fb158eb1f8821a9de11c00f3f092d8
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2019-08-03 09:35:17 +0200

    Repositories service + unit tests

--- src/repositories/repositories.service.spec.ts
+++ src/repositories/repositories.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RepositoriesService } from './repositories.service';
+import { FetcherService } from '../fetcher/fetcher.service';
+import { Request } from 'express';
 
 describe('RepositoriesService', (): void => {
     let service: RepositoriesService;
+    let fetchMock: jest.Mock = jest.fn();
 
     beforeEach(
         async (): Promise<void> => {
             const module: TestingModule = await Test.createTestingModule({
-                providers: [RepositoriesService]
+                providers: [
+                    RepositoriesService,
+                    {
+                        provide: FetcherService,
+                        useValue: {
+                            fetch: fetchMock,
+                            fetcher: { repository: { getProfile: jest.fn() } }
+                        }
+                    }
+                ]
             }).compile();
 
             service = module.get<RepositoriesService>(RepositoriesService);
@@ -16,5 +28,13 @@ describe('RepositoriesService', (): void => {
 
     it('should be defined', (): void => {
         expect(service).toBeDefined();
+    });
+
+    describe('getRepository', (): void => {
+        it('should call fetch method on FetcherService', async (): Promise<void> => {
+            await service.getRepository(({} as unknown) as Request, 'dummy-username', 'dummy-repository');
+
+            expect(fetchMock).toHaveBeenCalled();
+        });
     });
 });
--- src/repositories/repositories.service.ts
+++ src/repositories/repositories.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { FetcherService } from '../fetcher/fetcher.service';
+import { Request } from 'express';
+import { RepositoryProfile } from 'github-api-fetcher';
 
 @Injectable()
-export class RepositoriesService {}
+export class RepositoriesService {
+    private readonly fetcherService: FetcherService;
+
+    constructor(fetcherService: FetcherService) {
+        this.fetcherService = fetcherService;
+    }
+
+    async getRepository(req: Request, ownerName: string, repositoryName: string): Promise<RepositoryProfile | null> {
+        return await this.fetcherService.fetch(
+            req,
+            this.fetcherService.fetcher.repository.getProfile(ownerName, repositoryName)
+        );
+    }
+}
