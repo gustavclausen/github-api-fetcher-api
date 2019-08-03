8f5c7fbe1df497cb77eb2c5e64662e54468d12e9
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2019-08-03 09:20:17 +0200

    Organizations controller + unit tests

--- src/organizations/organizations.controller.spec.ts
+++ src/organizations/organizations.controller.spec.ts
@@ -1,20 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganizationsController } from './organizations.controller';
+import { OrganizationsService } from './organizations.service';
+import { FetcherService } from '../fetcher/fetcher.service';
+import { Request } from 'express';
+import { NotFoundException } from '@nestjs/common';
+import { OrganizationProfile } from 'github-api-fetcher';
 
 describe('Organizations Controller', (): void => {
     let controller: OrganizationsController;
+    let organizationsService: OrganizationsService;
 
     beforeEach(
         async (): Promise<void> => {
             const module: TestingModule = await Test.createTestingModule({
-                controllers: [OrganizationsController]
+                controllers: [OrganizationsController],
+                providers: [
+                    OrganizationsService,
+                    {
+                        provide: FetcherService,
+                        useValue: { fetch: jest.fn() }
+                    }
+                ]
             }).compile();
 
             controller = module.get<OrganizationsController>(OrganizationsController);
+            organizationsService = module.get<OrganizationsService>(OrganizationsService);
         }
     );
 
     it('should be defined', (): void => {
         expect(controller).toBeDefined();
+    });
+
+    describe('getOrganization', (): void => {
+        it('should throw NotFoundException when OrganizationsService returns null', async (): Promise<void> => {
+            jest.spyOn(organizationsService, 'getOrganization').mockReturnValue(
+                new Promise((resolve): void => resolve(null))
+            );
+
+            await expect(controller.getOrganization(({} as unknown) as Request, 'dummy-username')).rejects.toThrowError(
+                NotFoundException
+            );
+        });
+
+        it('should return result from OrganizationsService', async (): Promise<void> => {
+            const organzationName = 'test-organzation';
+            const returnValue = ({ name: organzationName } as unknown) as OrganizationProfile;
+
+            jest.spyOn(organizationsService, 'getOrganization').mockReturnValue(
+                new Promise(
+                    (resolve): void => {
+                        resolve(returnValue);
+                    }
+                )
+            );
+
+            expect(await controller.getOrganization(({} as unknown) as Request, organzationName)).toBe(returnValue);
+        });
     });
 });
--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Param, NotFoundException } from '@nestjs/common';
+import { OrganizationsService } from './organizations.service';
+import { Request } from 'express';
+import { OrganizationProfile } from 'github-api-fetcher';
 
 @Controller('organizations')
-export class OrganizationsController {}
+export class OrganizationsController {
+    private readonly organizationsService: OrganizationsService;
+
+    constructor(organizationsService: OrganizationsService) {
+        this.organizationsService = organizationsService;
+    }
+
+    @Get(':organizationName')
+    async getOrganization(
+        @Req() req: Request,
+        @Param('organizationName') organizationName: string
+    ): Promise<OrganizationProfile> {
+        const profile = await this.organizationsService.getOrganization(req, organizationName);
+
+        if (!profile) throw new NotFoundException(`GitHub organization '${organizationName} not found'`);
+
+        return profile;
+    }
+}
