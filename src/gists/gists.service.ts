e98607ec7a91ac4fb2ce7bdadf6ec47cf6cc5526
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2019-08-04 18:45:34 +0200

    Initial gists service + unit tests

--- src/gists/gists.service.ts
+++ src/gists/gists.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { FetcherService } from '../fetcher/fetcher.service';
+import { Request } from 'express';
+import { GistProfile } from 'github-api-fetcher';
 
 @Injectable()
-export class GistsService {}
+export class GistsService {
+    private readonly fetcherService: FetcherService;
+
+    constructor(fetcherService: FetcherService) {
+        this.fetcherService = fetcherService;
+    }
+
+    async getGist(req: Request, ownerUsername: string, gistName: string): Promise<GistProfile | null> {
+        return await this.fetcherService.fetch(
+            req,
+            this.fetcherService.fetcher.gist.getProfile(ownerUsername, gistName)
+        );
+    }
+}
